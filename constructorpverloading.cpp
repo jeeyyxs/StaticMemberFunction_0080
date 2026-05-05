fc09940064ceeae4c889278c547661f2238cd3e2
jeeyyxs <ajengrahmadhani02@gmail.com>
2026-05-05 18:12:17 +0700

    membuat class mahasiswa

--- constructorpverloading.cpp
+++ constructorpverloading.cpp
@@ -1,3 +1,31 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class mahasiswa{
+private:
+    int nim;
+    string nama;
+    public:
+    mahasiswa();
+    mahasiswa(int);
+    mahasiswa(string);
+    mahasiswa(int iNim, string iNama);
+    void cetak();
+};
+
+mahasiswa :: mahasiswa(){
+}
+
+mahasiswa :: mahasiswa(int iNim){
+    nim = iNim; // definisi hanya NIM
+}
+
+mahasiswa :: mahasiswa(string iNama){
+    nama = iNama; // definisi hanya NAMA
+}
+
+mahasiswa :: mahasiswa(int iNim, string iNama){
+    nim = iNim; // definisi NIM dan NAMA
+    nama = iNama;
+}
