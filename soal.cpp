c0728d9228cb9f3ba6744f87bbf989450eae2a07
jeeyyxs <ajengrahmadhani02@gmail.com>
2026-05-05 18:45:29 +0700

    library

---
+++ soal.cpp
@@ -0,0 +1,13 @@
+// Buatlah sebuah program C++ sederhana dengan constructor pada class Produk,
+// di mana diminta untuk mengelola data inventaris toko yang memiliki
+// atribut private berupa id produk dan nama produk dengan menyediakan
+// empat metode inisialisasi objek, pertama menggunakan constructor tanpa parameter untuk nilai default,
+// kedua hanya dengan parameter id, ketiga hanya dengan parameter nama, dan
+// keempat dengan parameter lengkap, lalu tampilkan untuk menyajikan informasi produk yang telah dibuat sebelumnya.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
