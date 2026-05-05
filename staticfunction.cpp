dc140d864e1cb5d52251cac18ba1b47f2c4c9ff2
jeeyyxs <ajengrahmadhani02@gmail.com>
2026-05-05 17:40:53 +0700

    membuat class mahasiswa dan static function

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -1,3 +1,29 @@
 #include <iostream>
 #include <string> //menhyertakan library string
 using namespace std;
+
+class Mahasiswa {
+
+private:
+    static int nim;
+public:
+    int id;
+    string nama;
+
+    void setIDC();
+    void printALL();
+
+    //pembuatan static function
+    static void setNim(int pnim) {
+        nim = pnim;
+    };
+
+    static int getNimC(){
+        return nim;
+    }
+
+    Mahasiswa(string pnama) :nama(pnama) {
+        setID();
+    };
+};
+            
