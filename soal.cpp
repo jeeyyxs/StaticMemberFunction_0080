46b6adf05130ffc054235ea0e0e4f73e7bb51091
jeeyyxs <ajengrahmadhani02@gmail.com>
2026-05-05 18:50:58 +0700

    menyelesaikan soal

--- soal.cpp
+++ soal.cpp
@@ -9,5 +9,51 @@
 #include <string>
 using namespace std;
 
+class Produk {
+private:
+    int id;
+    string nama;
 
+public:
 
+    void tampilkan() {
+        cout << "ID: " << id << endl;
+        cout << "Nama: " << nama << endl;
+        cout << endl;
+    }
+
+    Produk (){
+        id = 14;
+        nama = "Baju";
+    }
+
+    Produk (int Pid){
+        id=Pid;
+        nama = "Meja";
+    }
+
+    Produk (string Pnama){
+        id = 1;
+        nama=Pnama;
+    }
+
+    Produk (int Pid,string Pnama){
+        id=Pid;
+        nama=Pnama;
+    }
+};
+
+int main() {
+    cout << "- Daftar Inventaris Toko -" << endl;
+    Produk Produk1;
+    Produk Produk2 (3);
+    Produk Produk3 ("Meja");
+    Produk Produk4 (4,"Kursi");
+
+    Produk1.tampilkan();
+    Produk2.tampilkan();
+    Produk3.tampilkan();
+    Produk4.tampilkan();
+
+    return 0;
+}
