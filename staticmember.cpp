7ece44dd83f2fb9fd1d08bfa35b936b56c060bb8
jeeyyxs <ajengrahmadhani02@gmail.com>
2026-05-05 17:19:23 +0700

    membuat class mahasiswa

--- staticmember.cpp
+++ staticmember.cpp
@@ -1,3 +1,26 @@
 #include <iostream>
 using namespace std;
 
+class Mahasiswa{//membuat class mahasiswa
+   
+public:
+        static int nim; //deklarasi variabel static yaitu variabel nim
+        //deklarasi variabel member
+        int id;
+        string nama;
+
+        //deklarasi prosedur setID() dan printALLO)
+        void setID();
+        void printALL();
+
+        //pembuatan constructor Mahasiswa dengan parameter pnama
+        //Member initialize list
+        Mahasiswa(string pnama) :nama(pnama) {
+            setID();
+        }
+};
+
+
+
+    
+    
